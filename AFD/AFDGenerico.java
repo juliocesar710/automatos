f891ae364c406e82dc5d20822ad7cff703948a28
Julio <juliocandeia594@gmail.com>
2024-04-07 18:39:58 -0300

    Colocando um AFD generico

---
+++ AFD/AFDGenerico.java
@@ -0,0 +1,52 @@
+
+
+import java.util.Scanner;
+
+public class AFDGenerico {
+
+    static int[][] transicao = {{0,1},{1,0}};
+    static int estadoInicial = 0;
+    static int[] aceitacao = {1};
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            String cadeia;
+            int posicao=0,estado=estadoInicial;
+            char simbolo;
+
+            System.out.print("Digite uma cadeia do alfabeto:");
+            cadeia = scan.nextLine();
+
+
+            while(posicao<cadeia.length()){
+
+                imprimeCI(cadeia, estado, posicao);
+                simbolo = cadeia.charAt(posicao);
+                int simboloInt = Integer.parseInt(simbolo+"");
+                estado = transicao[estado][simboloInt];
+                posicao++;
+
+            }
+            imprimeCI(cadeia, estado, posicao);
+
+            boolean aceita = false;
+            for(int i:aceitacao){
+                if(estado==i) aceita=true;
+            }
+            if(aceita){
+                System.out.println("Aceita");
+            }
+            else {
+                System.out.println("Não Aceita");   
+            }
+
+           
+        }
+// CI = Configuracao Instantanea
+
+        public static void imprimeCI(String cadeia, int estado, int posicao){
+                System.out.print(cadeia.substring(0, posicao));
+                System.out.print("[q"+estado+"]");
+                System.out.println(cadeia.substring(posicao));
+        }
+        
+    }
